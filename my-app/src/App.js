d9da442a07e5f7ac2b5592255a3a7b218dc0194b
chaerul anwar <chaerulanwar428@gmail.com>
2023-03-08 14:48:59 +0700

    mengambil api movie

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,22 +1,41 @@
 import './App.css';
-
-
-
+import {getMovieList} from './api';
+import { useEffect,useState } from 'react';
 
 const App = () => {
+ 
+  const [popularMovies, setPopularMovies] = useState([])
+
+  useEffect(() => {
+    getMovieList().then((result) =>{
+      setPopularMovies(result)
+    })
+  },[])
+
+ const PopularMovieList = () =>{
+  return popularMovies.map((movie,i) =>{
+  return(
+      <div className="Movie-wrapper" key={i}>
+        <div className="Movie-title">{movie.title}</div>
+        <img className="Movie-image" src={movie.poster_path}/>
+        <div className="Movie-date">{movie.release_date}</div>
+        <div className="Movie-rate">{movie.vote_averange}</div>
+      </div>
+  )
+  })
+ }
+
  const search = (q) => {
   console.log({q})
  }
+
   return (
-    <div classname="App">
+    <div className="App">
       <header className="App-header">
       <h1>CR movie</h1>
-      <div className="Movie-container">
         <input placeholder="cari film favoritmu disini..." className="Movie-search" 
         onChange={({target}) => search(target.value)}/>
-        <img className="Movie-title"/>
-        <div className="Movie-date"></div>
-        <div className="Movie-rate"></div>
+      <div className="Movie-container">
       </div>
       </header>
     </div>
