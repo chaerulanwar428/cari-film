8eac2062c39f45bcfcd81f8319ea3a04558107cc
chaerul anwar <chaerulanwar428@gmail.com>
2023-03-09 19:11:35 +0700

    aplikasi selesai dibuat

--- my-app/src/api.js
+++ my-app/src/api.js
@@ -2,11 +2,11 @@ import axios from 'axios';
 
 export const getMovieList = async () =>{
     const movie = await axios.get(
-        `${process.env.REACT_APP_BASEURL}/movie/popular?api_key=${process.env.REACT_APP_APIKEY}`)
+        `${process.env.REACT_APP_BASEURL}/movie/popular?page=1&api_key=${process.env.REACT_APP_APIKEY}`)
     return movie.data.results
 }
 
 export const searchMovie = async (q) => {
-    const search = await axios.get(`${process.env.REACT_APP_BASEURL}/search/movie?query=${q}&api_key=${process.env.REACT_APP_APIKEY}`)
+    const search = await axios.get(`${process.env.REACT_APP_BASEURL}/search/movie?query=${q}&page=1&api_key=${process.env.REACT_APP_APIKEY}`)
     return search.data
 }
